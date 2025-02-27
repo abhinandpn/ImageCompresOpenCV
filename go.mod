97a5f35571d5878cbb7d05ec01d1034aaabdebf6
abhinandpn <abhinandpn4067@gmail.com>
2025-02-27 18:18:02 +0530

    project full changing

--- go.mod
+++ go.mod
@@ -1,9 +1,3 @@
-module github.com/abhinandpn/ImageCompresOpenCV
+module github.com/abhinandpn/ImageCompress
 
 go 1.23.6
-
-require (
-	github.com/cepot-blip/opencv-go-project v0.0.0-20240319093054-f8c1eeca1293
-	github.com/gorilla/mux v1.8.1
-	gocv.io/x/gocv v0.40.0
-)
--- main.go
+++ main.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"log"
-	"net/http"
-
-	"github.com/abhinandpn/ImageCompresOpenCV/handler"
-	"github.com/gorilla/mux"
-)
-
 func main() {
-	// INITIALIZE THE ROUTER USING MUX.NEWROUTER()
-	router := mux.NewRouter()
-	router.HandleFunc("/convert", handler.ConvertHandler).Methods("POST")
-	router.HandleFunc("/resize", handler.ResizeHandler).Methods("POST")
-	router.HandleFunc("/compress", handler.CompressHandler).Methods("POST")
 
-	// SPECIFY THE SERVER ADDRESS
-	serverAddr := ":8080"
-
-	// PRINT A MESSAGE TO THE CONSOLE THAT THE SERVER IS STARTING AND LISTENING ON A SPECIFIC ADDRESS
-	log.Printf("Server is starting and listening on %s...", serverAddr)
-
-	// USE HTTP.LISTENANDSERVE() TO START THE SERVER WITH THE PREVIOUSLY DEFINED ROUTER
-	log.Fatal(http.ListenAndServe(serverAddr, router))
 }
